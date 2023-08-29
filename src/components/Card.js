290bd42c537036532ae7b6cc83b23644e344cc45
Francisco Berti da Cruz <fbc991@live.com>
2023-08-29 16:56:20 -0300

    .

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import { GithubContext } from '../context/context';
+import { GithubContext, useGithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const {
+    githubUser: {
+      avatar_url,
+      html_url,
+      name,
+      company,
+      blog,
+      bio,
+      location,
+      twitter_username,
+    },
+  } = useGithubContext();
+
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || 'default value'}</p>
+        </div>
+        <a href={html_url} target="_blank" rel="noopener noreferrer">
+          follow
+        </a>
+      </header>
+      <p className="bio">{bio}</p>
+      <div className="links">
+        <p>
+          <MdBusiness />
+          {company}
+        </p>
+        <p>
+          <MdLocationOn />
+          {location}
+        </p>
+        <a href={`https://${blog}`} target="_blank" rel="noopener noreferrer">
+          <MdLink />
+          {blog}
+        </a>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
