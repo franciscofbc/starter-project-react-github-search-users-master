87651d37f9d1cc9b8c0b352f062497c67bffd8f2
Francisco Berti da Cruz <fbc991@live.com>
2023-09-11 16:26:03 -0300

    ...

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
-import { GithubContext } from '../context/context';
+import { GithubContext, useGithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+
+const chartData = [
+  {
+    label: 'Venezuela',
+    value: '35',
+  },
+  {
+    label: 'Saudi',
+    value: '16',
+  },
+  {
+    label: 'Canada',
+    value: '89',
+  },
+];
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useGithubContext();
+
+  let languages = repos.reduce((acc, repo) => {
+    const { language } = repo;
+    if (!language) return acc;
+    if (!acc[language]) {
+      acc[language] = { label: language, value: 1 };
+    } else {
+      acc[language] = {
+        ...acc[language],
+        value: acc[language].value + 1,
+      };
+    }
+    return acc;
+  }, {});
+
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData} />; */}
+        <Pie3D data={languages} />;
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
